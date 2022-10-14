6e4028162c0e67467dc65a163863573ca58a25fc
dacho <dosenovicdavid7@gmail.com>
2022-10-14 23:09:26 +0200

    Spring Boot Project Created

---
+++ src/test/java/com/thales/qrapi/QrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thales.qrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
