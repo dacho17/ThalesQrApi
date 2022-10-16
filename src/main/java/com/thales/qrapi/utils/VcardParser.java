47a6f9018a6508ea9c0d9338e8f70c5b3f5a461b
dacho <dosenovicdavid7@gmail.com>
2022-10-16 23:34:18 +0200

    Vcard Parser Implemented

---
+++ src/main/java/com/thales/qrapi/utils/VcardParser.java
@@ -0,0 +1,22 @@
+package com.thales.qrapi.utils;
+
+import org.springframework.stereotype.Component;
+
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+
+@Component
+public class VcardParser {
+
+	// if no vcard is found in the qrcode content, return null and handle the case in a non-vcard way in the calling func.
+	public String parseStringContent(String strContent) {
+		VCard vcard = Ezvcard.parse(strContent).first();
+		
+		if (vcard == null) {
+			return null;
+		}
+		
+		String xmlInStringFormat = Ezvcard.writeXml(vcard).go();
+		return xmlInStringFormat;
+	}
+}
