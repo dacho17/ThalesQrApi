c954a440337b5445ca9491eda98150387c3ab2a2
dacho <dosenovicdavid7@gmail.com>
2022-10-17 20:21:27 +0200

    QrCode Service Commit After Refactoring

--- src/main/java/com/thales/qrapi/services/QrCodeApiService.java
+++ src/main/java/com/thales/qrapi/services/QrCodeApiService.java
@@ -1,7 +1,6 @@
 package com.thales.qrapi.services;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,11 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.thales.qrapi.dtos.enums.QrCodeContentType;
-import com.thales.qrapi.dtos.qrcode.QrCodeContentHolder;
 import com.thales.qrapi.dtos.qrcode.QrCodeDto;
 import com.thales.qrapi.entities.QrCode;
-import com.thales.qrapi.entities.Vcard;
 import com.thales.qrapi.exceptions.BadRequestApiException;
 import com.thales.qrapi.exceptions.DbApiException;
 import com.thales.qrapi.exceptions.NotFoundApiException;
@@ -27,7 +23,7 @@ import com.thales.qrapi.utils.QrCodeHelper;
 @Service
 public class QrCodeApiService implements QrCodeService<String, QrCodeDto> {
 
-	private static final String findInDbError = "An exception has occured while finding the entry in the database.";
+	private static final String dbError = "An exception has occured in communication with the database.";
 	private static final String notFoundInDbError = "No instance with the provided id has been found.";
 	private static final String illegalArgumentsProvided = "No, or illegal arguments have been provided to the endpoint.";
 	private static final String errorReadingFile = "An error has occured while processing the uploaded file.";
@@ -47,9 +43,7 @@ public class QrCodeApiService implements QrCodeService<String, QrCodeDto> {
 	public String save(MultipartFile file) throws BadRequestApiException, ServerApiException, DbApiException {
 		try {
 			byte[] bytes = file.getBytes();
-			QrCodeContentHolder contentHolder = qrCodeHelper.extractQrCodeData(bytes);
-			
-			QrCode newQrCode = generateNewQrCode(bytes, file, contentHolder);
+			QrCode newQrCode = qrCodeHelper.generateNewQrCode(bytes, file);
 
 			qrCodeRepo.save(newQrCode);
 			return qrCodeMapper.encodeId(newQrCode.getId());
@@ -64,7 +58,7 @@ public class QrCodeApiService implements QrCodeService<String, QrCodeDto> {
 			throw new BadRequestApiException(exc.getMessage());
 		} catch(Exception exc) {
 			exc.printStackTrace();
-			throw new DbApiException(findInDbError);
+			throw new DbApiException(dbError);
 		}
 	}
 
@@ -80,7 +74,7 @@ public class QrCodeApiService implements QrCodeService<String, QrCodeDto> {
 			return res;
 		} catch (Exception exc) {
 			exc.printStackTrace();
-			throw new DbApiException(findInDbError);
+			throw new DbApiException(dbError);
 		}
 	}
 
@@ -105,7 +99,7 @@ public class QrCodeApiService implements QrCodeService<String, QrCodeDto> {
 			throw new NotFoundApiException(exc.getMessage());
 		} catch(Exception exc) {
 			exc.printStackTrace();
-			throw new DbApiException(findInDbError);
+			throw new DbApiException(dbError);
 		}
 		
 		if (!qrCode.isPresent()) {
@@ -143,44 +137,9 @@ public class QrCodeApiService implements QrCodeService<String, QrCodeDto> {
 			throw new NotFoundApiException(exc.getMessage());
 		} catch(Exception exc) {
 			exc.printStackTrace();
-			throw new DbApiException(findInDbError);
+			throw new DbApiException(dbError);
 		}
 		
 		return id;
-	}
-
-	
-	// Private Service Methods
-	private QrCode generateNewQrCode(byte[] bytes, MultipartFile file, QrCodeContentHolder contentHolder) {
-		String fileName = file.getOriginalFilename();
-		
-		// NOTE: code that will get me an authenticated user
-//		Authentication authentication = 
-//		        authenticationManager.authenticate(
-//		            new UsernamePasswordAuthenticationToken(username, password)
-//		        );
-//
-//		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-		
-		QrCode newQrCode = new QrCode(
-			fileName,
-			contentHolder.getContentType().getValue(),
-			bytes,
-			contentHolder.getContent(),
-			"DavidTODO",	// TODO: username of the authenticated account must be inserted here
-			new Timestamp(System.currentTimeMillis())
-		);
-		
-		if (QrCodeContentType.getType(newQrCode.getContentType()) == QrCodeContentType.VCARD)
-			setVcardQrCodeContent(newQrCode, contentHolder);
-		
-		return newQrCode;
-	}
-	
-	private void setVcardQrCodeContent(QrCode qrCode, QrCodeContentHolder contentHolder) {
-		Vcard vCard = new Vcard(contentHolder.getContent());
-		
-		qrCode.setTextContent(null);	// qrCode which carries vCard has a content in a separate table
-		qrCode.setvCard(vCard); 		// the content is stored in vCard table
 	}
 }
